aeaa6ef433144e3906fc140e0d183c90230ecd2c
John Nazarene <jannazarene09@gmail.com>
2025-01-08 17:30:14 +0800

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+  /* 
   cloud {
-    organization = "organization-name"
+    organization = "hcp_test_organization"
 
     workspaces {
       name = "learn-terraform"
     }
   }
-
+ */
   required_providers {
     aws = {
       source  = "hashicorp/aws"
